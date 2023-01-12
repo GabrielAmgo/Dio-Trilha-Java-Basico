1bdb1f750a9c8bf9271f37f77d79e646529e9a92
GabrielAmgo <gabrielp.amigo@gmail.com>
2023-01-12 16:55:22 -0300

    Criado a classe para  estudar operadores e metodos

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,9 @@
+public class Operadores {
+    public static void main(String [] args) throws Exception {
+        int rightQuestions = 6;
+
+    String grade = rightQuestions>=6 ? "got it" : "failed"; // Ternary operator example
+        System.out.println(grade);
+    
+    }
+}
