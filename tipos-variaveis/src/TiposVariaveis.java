a24aca34761ce82e9f920f616765862cd4429c56
GabrielAmgo <gabrielp.amigo@gmail.com>
2023-01-12 15:54:34 -0300

    Variáveis e seus tipos de valores

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,6 +1,17 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        short ano = 127;
+        byte idade = 127;
+        short ano = 2023;
+        int cep = 423211;
+        long cpf = 9999999999l;
+        float pi = 3.14F;
+        double salario = 2359.90;
+        
+        System.out.println(salario);
+        System.out.println(pi);
+        System.out.println(cpf);
+        System.out.println(cep);
+        System.out.println(idade);
         System.out.println(ano);
     }
 }
